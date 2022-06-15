1ec7f19c0ef1107597c34c979cdf2e6907b2dec0
ALowton <ALowton@QA.com>
2022-06-15 15:35:33 +0100

    AL: conditionals

--- src/com/qa/operators/Operators.java
+++ src/com/qa/operators/Operators.java
@@ -13,4 +13,47 @@ public class Operators {
 	public void modulus(int numOne, int numTwo) {
 		System.out.println(numOne % numTwo);
 	}
+	
+	// += 
+	public void plusEquals() { 
+		int num = 10;
+		
+		num += 5; // num = num + 5
+		
+		System.out.println(num);
+	}
+	
+	// -=
+	public void minusEquals() { 
+		int num = 10;
+		
+		num -= 5; // num = num - 5
+		
+		System.out.println(num);
+	}
+	
+	// increment
+	public void increment() {
+		int num = 10;
+		
+		num++;
+		
+		System.out.println(num);
+	}
+	
+	// decrement
+	public void decrement() {
+		int num = 10;
+		
+		num--;
+		
+		System.out.println(num);
+	}
+	
+	// ! - not operator (inverse)
+	public void notOperator() {
+		boolean flag = true;
+		
+		System.out.println(!flag);
+	}
 }
--- src/com/qa/operators/Runner.java
+++ src/com/qa/operators/Runner.java
@@ -8,13 +8,14 @@ public class Runner {
 		
 		// Use the object to call the methods
 		op.basicMaths(10, 5);
-		op.basicMaths(4, 5);
-		op.basicMaths(11, 4);
-		op.basicMaths(8, 5);
-		
 		op.modulus(9, 4);
-		op.modulus(10, 4);
-		op.modulus(13, 5);
-		op.modulus(11, 4);
+		
+		op.plusEquals();
+		op.minusEquals();
+		
+		op.increment();
+		op.decrement();
+		
+		op.notOperator();
 	}
 }
