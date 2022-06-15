1e109fbdd9a3bd23c99cf1e4259e8ab9cb3e1a90
ALowton <ALowton@QA.com>
2022-06-15 16:29:33 +0100

    AL: added results exercise

---
+++ src/com/qa/operators/results/Runner.java
@@ -0,0 +1,11 @@
+package com.qa.operators.results;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Results res = new Results(120, 90, 100);
+
+		res.methodOne();
+		res.methodTwo();
+	}
+}
