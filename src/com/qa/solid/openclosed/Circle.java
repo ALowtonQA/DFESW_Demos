f70296fcc375dfe494bf58de7f3b16fb36ee8062
ALowton <ALowton@QA.com>
2022-06-30 14:49:30 +0100

    AL: open/closed

---
+++ src/com/qa/solid/openclosed/Circle.java
@@ -0,0 +1,25 @@
+package com.qa.solid.openclosed;
+
+// POJO - Plain old java object
+public class Circle implements Shape{
+
+	private double radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double calculateArea() {
+		return getRadius() * getRadius() * Math.PI;
+	}
+	
+}
